1fe37f65a03dce9f80b52428b81a81b138a36121
Timur Gain <timur.gain@gmail.com>
2024-04-09 21:05:50 +0400

    feat: CarCreate, add react-hook-form validation, default color picker

--- src/features/CarCreate/ui/CarCreate.tsx
+++ src/features/CarCreate/ui/CarCreate.tsx
@@ -1,18 +1,62 @@
-import { Input, InputKits } from '@/shared/ui/Input/Input';
 import styles from './CarCreate.module.scss';
-import { Button, ButtonKits } from '@/shared/ui/Button/Button';
 import clsx from 'clsx';
+import { Input, InputKits, InputTypes } from '@/shared/ui/Input/Input';
+import { Button, ButtonKits } from '@/shared/ui/Button/Button';
+import { useForm } from 'react-hook-form';
+import { InputNames, FormData } from '../types/types';
+import { validationOptions } from '@/shared/utils/validationForm';
 
 type Props = {
   className?: string;
 };
 
-export function CarCreate({className}: Props) {
+export function CarCreate({ className }: Props) {
+  // 0. Config
+
+  const defaultFormValues = {
+    [InputNames.NAME]: '',
+    [InputNames.COLOR]: '#ffffff',
+  };
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isValid },
+  } = useForm<FormData>({
+    mode: 'onChange',
+    criteriaMode: 'all',
+    defaultValues: defaultFormValues,
+  });
+
+  function createCar(data: FormData) {
+    console.log('CarCreate', data);
+    reset(defaultFormValues);
+  }
+
+  // Render
+
   return (
-    <form className={clsx(styles.form, className)}>
-      <Input kit={InputKits.PRINARY_M} placeholder="TYPE CAR BRAND" />
-      {/* ColorPickerTrigger */}
-      <Button kit={ButtonKits.PRYMARY_M_PURPLE}>CREATE</Button>
+    <form className={clsx(styles.form, className)} onSubmit={handleSubmit(createCar)}>
+      <Input
+        kit={InputKits.PRINARY_M}
+        placeholder="TYPE CAR BRAND"
+        type={InputTypes.TEXT}
+        register={register}
+        options={validationOptions.TEXT_REQUIRED}
+        name={InputNames.NAME}
+        validationError={errors[InputNames.NAME]?.message}
+      />
+      <Input
+        kit={InputKits.COLOR_M}
+        type={InputTypes.COLOR}
+        register={register}
+        options={validationOptions.COLOR_REQUIRED}
+        name={InputNames.COLOR}
+      />
+      <Button kit={ButtonKits.PRYMARY_M_PURPLE} type="submit" disabled={!isValid}>
+        CREATE
+      </Button>
     </form>
   );
 }
