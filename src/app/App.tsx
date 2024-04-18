83bd4ba4be9e5eca45b00ef88385b419658b32b7
Timur Gain <timur.gain@gmail.com>
2024-04-18 20:32:36 +0400

    feat: add basic 404 route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,19 +5,15 @@ import WinnersPage from '../pages/WinnersPage';
 import routes from './routes';
 
 export default function App(): React.ReactNode {
-
-  // TODO:
-  // - errorBoundary
-  // - 404 page
-  // - car animation
-  // - Modal if there is no connction to the server
-  // - Jest tests
-
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         <Route path={routes.garage} element={<GaragePage />} />
         <Route path={routes.winners} element={<WinnersPage />} />
+        <Route
+          path="*"
+          element={<div style={{ color: 'yellow', fontSize: '36px', margin: 'auto' }}>404</div>}
+        />
       </Routes>
     </Suspense>
   );
